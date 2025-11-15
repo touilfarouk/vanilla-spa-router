301d57eccf626fec8c1330ea0342405add0a0a44
touilfarouk <farouktouil@hotmail.com>
2025-11-15 10:31:48 +0100

    add a css simple cdn

--- js/about.js
+++ js/about.js
@@ -3,9 +3,9 @@ function init(params = {}) {
     const values = Object.values(params);
     const userEl = document.getElementById("user-id");
     const postEl = document.getElementById("post-id");
-    
     userEl && (userEl.textContent = values[0] ? `ID: ${values[0]}` : "No user selected");
     postEl && (postEl.textContent = `Post ID: ${values[2] || ''}`, postEl.style.display = values[2] ? 'block' : 'none');
+    
 }
 
 export { init };
